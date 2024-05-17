12563239b70039b0185fce19a521dec0d02baca4
RiosEunice <163943499+RiosEunice@users.noreply.github.com>
2024-05-17 11:17:48 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue miFila=new Queue();
+            Console.Write("Ingrese la cantidad de Filas: ");
+            int n = int.Parse(Console.ReadLine());
+            int tamañoFila = n;
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Nombre de la Persona: ");
+                miFila.Enqueue(Console.ReadLine());
+
+            }
+            Console.WriteLine("La cajera atendio a ");
+            while (tamañoFila > 0)
+            {
+                Console.WriteLine(miFila.Dequeue());
+                tamañoFila--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
